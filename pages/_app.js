c967b2349b6e4b07255454368d0c825d277ca7af
Fenando Becerra <fern>
2021-06-15 19:09:26 -0700

    Title

--- pages/_app.js
+++ pages/_app.js
@@ -78,7 +78,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>NextJS Material Kit by Creative Tim</title>
+          <title>O1SLeft Aloha Angelz</title>
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
